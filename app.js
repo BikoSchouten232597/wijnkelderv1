0d0bffd10ac2f71e5494541cfa03de06358ec6f1
BikoSchouten232597 <34523851+BikoSchouten232597@users.noreply.github.com>
2025-11-04 14:27:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // API CONFIGURATION
 // ============================================================================
 const API_CONFIG = {
-  baseURL: 'http://31.97.34.22:3001',
+  baseURL: 'http://wijndb.schoutendigital.com',
   timeout: 5000,
   endpoints: {
     wines: '/wines',
